7755707dbc7a13262929158ee98c2355781689a2
Beker-Dev <joaopedro.beker@hotmail.com>
2022-11-11 21:02:07 -0300

    changed login appearance

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,15 +1,16 @@
 import './Login.css'
+
 function Login() {
   return (
         <main id="main">
-          
-                <div id="login-card">
-                    <form id="form-login">
-                        <input type={"text"} placeholder="Email"></input>
-                        <input type={"password"} placeholder="Senha"></input>    
+                <div id="login_card">
+                    <h2 id="login_title">Carteira Digital</h2>
+                    <form id="login_form">
+                        <input type={"email"} placeholder="Email" id="login_field"></input>
+                        <input type={"password"} placeholder="Senha" id="login_field"></input>
+                        <input id="login_btn" type="button" value="Entrar"></input>
                     </form>  
                 </div>
-
         </main>
   );
 }
