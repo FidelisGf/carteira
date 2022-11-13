6d86d11ad08a5ed8f0606ab9230a7800bfc0769f
lucasbagrt <lucasghank@hotmail.com>
2022-11-13 18:18:28 -0300

    alts

---
+++ src/Components/Project/ProjectForm.jsx
@@ -0,0 +1,14 @@
+import styles from "./ProjectForm.module.css";
+import Input from '../Form/Input'
+import TextField from '@mui/material/TextField';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { createStyles, Grid} from '@mui/material';
+
+function ProjectForm( { title, valueModel, } ) {
+  return (
+    <div></div>
+  );
+}
+export default ProjectForm;
