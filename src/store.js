28880c9f39ff6b66f7e64322c1be4181f02e9dd4
Gabriel Fidelis Pereira <jpinformaticafoz@gmail.com>
2022-11-19 13:45:41 -0300

    add_store

---
+++ src/store.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  valorTotal: null,
+  list : []
+}
+
+const slice = createSlice({
+  name: 'listReducer',
+  initialState,
+  reducers: {
+    addList: (state, action) => {
+      state.list.push(action.payload);
+    },
+    setValorTotal: (state, action) => {
+      state.valorTotal = action.payload
+    }
+  }
+});
+
+export const store = configureStore({
+  reducer: {
+    wallet : slice.reducer
+  }
+});
+
+export const { addList, setValorTotal} = slice.actions;
+
+export const useAppDispatch = () => useDispatch();
+
+export const useAppSelector = useSelector;
